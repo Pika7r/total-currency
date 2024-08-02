251ee59bd9ff0431cb6b5be0ce1dc7082d179e08
Pika7r <149111408+Pika7r@users.noreply.github.com>
2024-08-02 23:22:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function add() {
+    let a = document.getElementById("num1").value
+    let b = document.getElementById("num2").value
+    let c = document.getElementById("num3").value
+    let d = document.getElementById("num4").value
+    let e = document.getElementById("num5").value
+    let f = document.getElementById("num6").value
+    let g = document.getElementById("num7").value
+    let h = document.getElementById("num8").value
+    let i = document.getElementById("num9").value
+
+    let l=1
+    
+
+    a = a*parseInt(l)
+    b = b*parseInt(l)
+    c = c*parseInt(l)
+    d = d*parseInt(l)
+    e = e*parseInt(l)
+    f = f*parseInt(l)
+    g = g*parseInt(l)
+    h = h*parseInt(l)
+    i = i*parseInt(l)
+
+    a =parseInt(a) * 2000
+    b =parseInt(b) * 500
+    c =parseInt(c) * 100
+    d =parseInt(d) * 50
+    e =parseInt(e) * 20
+    f =parseInt(f) * 10
+    g =parseInt(g) * 5
+    h =parseInt(h) * 2
+    i =parseInt(i) * 1
+    let j = a + b + c + d + e + f + g + h + i
+    let m="₹"
+    let k = document.getElementById("num10").value = m+j
+
+    console.log(a + b)
+}
+
+let details = {
+    name: "Ravi",
+    Rollno: "1",
+    marks: "100"
+}
+
+
+let string = "Ravi1"
+
+const name = "lokmany"
+
